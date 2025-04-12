86eada7037520db06a27dc0bd2cc90f917ea4747
Gene Z <1225842905@qq.com>
2025-04-12 21:24:37 +0800

    build: 更新版本信息
    - 更新 FactoryMethodPattern 的 AssemblyInformationalVersion 属性
    - 更新 SingletonPattern 的 AssemblyInformationalVersion 属性

--- FactoryMethodPattern/obj/Debug/net8.0/FactoryMethodPattern.AssemblyInfo.cs
+++ FactoryMethodPattern/obj/Debug/net8.0/FactoryMethodPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FactoryMethodPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bbab1a2c8170deee62c5ad35e1fa38a49a2d1bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba336574d0ba42e5f2194ea2f4c69c975cf67db4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FactoryMethodPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FactoryMethodPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
